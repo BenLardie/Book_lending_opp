f98b7f7ff4d5fe8935b43e5c0e8a2bf9d638a788
Ben Lardie <benlardie@gmail.com>
2019-02-21 15:48:56 -0500

    created class Book

---
+++ book_lending.py
@@ -0,0 +1,13 @@
+import random
+from datetime import datetime
+
+
+class Book:
+    """A class describing a book"""
+
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+
+    
