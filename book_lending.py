ecf9fbb30fdce4a66b88081110136413f5f9e87a
Ben Lardie <benlardie@gmail.com>
2019-02-21 15:55:45 -0500

    created a couple of class instances

--- book_lending.py
+++ book_lending.py
@@ -5,9 +5,19 @@ from datetime import datetime
 class Book:
     """A class describing a book"""
 
+    on_shelf = []
+    on_loan = []
+
     def __init__(self, title, author, isbn):
         self.title = title
         self.author = author
         self.isbn = isbn
+
+    def create(self):
+        newbook = Book()
+        return self.on_shelf.append(newbook)
+
+    def browse(self):
+        random.choice(self.on_shelf)
 
     
